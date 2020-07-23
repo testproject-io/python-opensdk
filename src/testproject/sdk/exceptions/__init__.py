078e69ffda84232b2c4bd4d20a0aadc87bf2c635
Bas Dijkstra <bas@ontestautomation.com>
2020-07-23 09:51:56 +0200

    Aligned Agent connect error handling with Java SDK implementation
    Signed-off-by: Bas Dijkstra <bas@ontestautomation.com>

--- src/testproject/sdk/exceptions/__init__.py
+++ src/testproject/sdk/exceptions/__init__.py
@@ -1,3 +1,6 @@
 from .sdkexception import SdkException
+from .agentconnectexception import AgentConnectException
+from .invalidtokenexception import InvalidTokenException
+from .obsoleteversionexception import ObsoleteVersionException
 
-__all__ = ["SdkException"]
+__all__ = ["SdkException", "AgentConnectException", "InvalidTokenException", "ObsoleteVersionException"]
