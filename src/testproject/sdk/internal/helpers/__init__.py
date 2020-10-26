433c7bca6e64d447c5b6c9efbd2a26ae109640f3
Bas Dijkstra <bas@ontestautomation.com>
2020-10-26 12:58:27 +0100

    Add generic command executor to be used with generic driver
    Signed-off-by: Bas Dijkstra <bas@ontestautomation.com>

--- src/testproject/sdk/internal/helpers/__init__.py
+++ src/testproject/sdk/internal/helpers/__init__.py
@@ -1,9 +1,11 @@
 from .custom_command_executor import CustomCommandExecutor
 from .custom_appium_command_executor import CustomAppiumCommandExecutor
 from .reporting_command_executor import ReportingCommandExecutor
+from .generic_command_executor import GenericCommandExecutor
 
 __all__ = [
     "CustomCommandExecutor",
     "CustomAppiumCommandExecutor",
     "ReportingCommandExecutor",
+    "GenericCommandExecutor"
 ]
