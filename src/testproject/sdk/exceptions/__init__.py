1653d9c5fe43262bb7679ca6d11680dd15e0885b
Bas Dijkstra <bas@ontestautomation.com>
2020-11-20 08:53:42 +0100

    Add custom exception type to raise when Agent indicates that requested browser could not be found on local machine.
    Signed-off-by: Bas Dijkstra <bas@ontestautomation.com>

--- src/testproject/sdk/exceptions/__init__.py
+++ src/testproject/sdk/exceptions/__init__.py
@@ -2,5 +2,12 @@ from .sdkexception import SdkException
 from .agentconnectexception import AgentConnectException
 from .invalidtokenexception import InvalidTokenException
 from .obsoleteversionexception import ObsoleteVersionException
+from .missingbrowserexception import MissingBrowserException
 
-__all__ = ["SdkException", "AgentConnectException", "InvalidTokenException", "ObsoleteVersionException"]
+__all__ = [
+    "SdkException",
+    "AgentConnectException",
+    "InvalidTokenException",
+    "ObsoleteVersionException",
+    "MissingBrowserException",
+]
