4e8eb4df01b5f6ff30605eb77b6d61a53e5d4a48
Tzah Mazuz <tzah.mazuz@testproject.io>
2021-01-17 16:58:27 +0200

    Adding always_pass flag to the StepSettings class.
    A special case that should be handled inside the SDK.
    
    Signed-off-by: Tzah Mazuz <tzah.mazuz@testproject.io>

--- src/testproject/classes/step_settings.py
+++ src/testproject/classes/step_settings.py
@@ -18,10 +18,11 @@ from src.testproject.enums import SleepTimingType, TakeScreenshotConditionType
 class StepSettings:
     """Represents settings for automatic step reporting."""
     def __init__(self, sleep_time: int = 0, sleep_timing_type: SleepTimingType = None, timeout: int = -1,
-                 invert_result: bool = False,
+                 invert_result: bool = False, always_pass: bool = False,
                  screenshot_condition: TakeScreenshotConditionType = TakeScreenshotConditionType.Failure):
         self.sleep_time = sleep_time
         self.sleep_timing_type = sleep_timing_type
         self.timeout = timeout
+        self.always_pass = always_pass
         self.invert_result = invert_result
         self.screenshot_condition = screenshot_condition
