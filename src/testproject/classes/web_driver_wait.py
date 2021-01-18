01f7ac21b8236c627f3449fd0555382a31d9ebf8
Tzah Mazuz <tzah.mazuz@testproject.io>
2021-01-18 18:28:35 +0200

    Improving reporting of WebDriverWait steps and handling the TimeoutException that can be thrown.
    - until/until_not function can raise TimeoutException, this should be handled by this decorator class as well.
    - Generating the step message with the step helper.
    - Adding method inputs to the step report.
    
    Signed-off-by: Tzah Mazuz <tzah.mazuz@testproject.io>

--- src/testproject/classes/web_driver_wait.py
+++ src/testproject/classes/web_driver_wait.py
@@ -2,6 +2,7 @@ import os
 import json
 from typing import Union
 
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.wait import WebDriverWait
 
 from src.testproject.classes import DriverStepSettings, StepSettings
@@ -21,9 +22,6 @@ class TestProjectWebDriverWait(WebDriverWait):
     """
     def __init__(self, driver: Union[BaseDriver, Remote], timeout):
         super().__init__(driver, timeout)
-        self.step_helper = driver.command_executor.step_helper
-        self.step_settings = driver.command_executor.settings
-        self.session_id = driver.command_executor.agent_client.agent_session.session_id
 
     def until(self, method, message=''):
         """Executes the wrapping function for until."""
@@ -41,53 +39,63 @@ class TestProjectWebDriverWait(WebDriverWait):
         Based on the function's result and given method, it will report this Step with all the needed information.
         Returns the result of the executed function.
         """
+        timeout_exception = None
+        step_helper = self.driver.command_executor.step_helper
+        step_settings = self.driver.command_executor.settings
         # Disable reports
         self._driver.report().disable_reports(True)
+        # Handle driver timeout
+        step_helper.handle_timeout(timeout=step_settings.timeout)
+        # Handle sleep before
+        step_helper.handle_sleep(sleep_timing_type=step_settings.sleep_timing_type,
+                                 sleep_time=step_settings.sleep_time)
+        # Execute the function with default StepSettings.
         with DriverStepSettings(self._driver, StepSettings()):
-            # Handle driver timeout
-            self.step_helper.handle_timeout(timeout=self.step_settings.timeout)
-            # Handle sleep before
-            self.step_helper.handle_sleep(sleep_timing_type=self.step_settings.sleep_timing_type,
-                                          sleep_time=self.step_settings.sleep_time)
-            # Execute the function.
-            result = getattr(super(), function_name)(method, message)
-            # Handle sleep after
-            self.step_helper.handle_sleep(sleep_timing_type=self.step_settings.sleep_timing_type,
-                                          sleep_time=self.step_settings.sleep_time, step_executed=True)
-            # Handle invert result
-            result = not result if self.step_settings.invert_result else result
-
-            # Handle always pass
-            result = True if self.step_settings.always_pass else result
-
-            # Handle screenshot condition
-            screenshot = self.step_helper.take_screenshot(self.step_settings.screenshot_condition, result)
+            try:
+                result = getattr(super(), function_name)(method, message)
+            except TimeoutException as e:
+                result = False
+                timeout_exception = e
+        # Handle sleep after
+        step_helper.handle_sleep(sleep_timing_type=step_settings.sleep_timing_type,
+                                 sleep_time=step_settings.sleep_time, step_executed=True)
+        # Handle result
+        result, step_message = step_helper.handle_step_result(step_result=result,
+                                                              invert_result=step_settings.invert_result,
+                                                              always_pass=step_settings.always_pass)
+        # Handle screenshot condition
+        screenshot = step_helper.take_screenshot(step_settings.screenshot_condition, result)
 
         # Enable reports and report the step result with all execution details.
         self._driver.report().disable_reports(False)
-        result_str = (('Passed' if result else 'Failed')
-                      + (' - Step result inverted' if self.step_settings.invert_result else ''))
-        step_name, step_attributes = self.get_report_details(method)
+
+        # Inferring function name - until / until not
         function_name = ' '.join(function_name.split('_'))
+        # Getting all additional step information.
+        step_name, step_attributes = self.get_report_details(method)
         self._driver.report().step(description=f'Wait {function_name} {step_name}',
-                                   message=f'Step {result_str}{os.linesep}.'
-                                           f'Step attributes:{os.linesep}'
-                                           f'{step_attributes}',
+                                   message=f'{step_message}{os.linesep}',
                                    passed=result,
+                                   inputs=step_attributes,
                                    screenshot=screenshot)
+        # Always pass ignore result and thrown exception.
+        if step_settings.always_pass:
+            return True
+        # Raise exception if there was one.
+        if timeout_exception:
+            raise timeout_exception
         return result
 
     def get_report_details(self, method):
         """Returns the inferred report details.
 
         Step name is the method class's name, underscores are replaces with spaces.
-        Step attributes is all the method's attributes and their values.
+        Attributes dict is all the method's attributes and their values.
         """
         step_name = ' '.join(method.__class__.__name__.split('_'))
         attributes_dict = {attribute: json.dumps(getattr(method, attribute))
                            for attribute in self.get_user_attributes(method)}
-        step_attributes = json.dumps(attributes_dict, indent=4)
-        return step_name, step_attributes
+        return step_name, attributes_dict
 
     @staticmethod
     def get_user_attributes(cls, exclude_methods=True):
