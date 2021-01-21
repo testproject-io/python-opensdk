71d28ecfca2abcb906edad2dd79858d2bada5031
david_goichman <david.goichman@testproject.io>
2021-01-22 00:02:28 +0200

    Added Behave BDD test example.
    Added a feature file, step definitions for the feature and environmeny file with report_behave
    decorator set up.
    
    Signed-off-by: david_goichman <david.goichman@testproject.io>

---
+++ tests/examples/frameworks/behave/features/environment.py
@@ -0,0 +1,54 @@
+# Copyright 2021 TestProject (https://testproject.io)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from src.testproject.sdk.drivers import webdriver
+from src.testproject.decorator.behave_reporter import behave_reporter
+
+""" Executed once per test run: Before any features and scenarios are run.
+    Initialize the driver and start the session.
+"""
+
+
+@behave_reporter()
+def before_all(context):
+    context.driver = webdriver.Chrome(projectname="Python BDD", jobname="Behave")
+
+
+""" Executed after each step in the scenario.
+    Reports the test step.
+"""
+
+
+@behave_reporter(screenshot=True)
+def after_step(context, step):
+    pass
+
+
+""" Executed after each scenario in the feature.
+    Reports the scenario as a test.
+"""
+
+
+@behave_reporter
+def after_scenario(context, scenario):
+    pass
+
+
+""" Executed once per test run: after all features and scenarios are run.
+    Quit the driver and close the session.
+"""
+
+
+def after_all(context):
+    context.driver.quit()
