0cd67368b7a7d781019ecd95d6b90e25711b1fb1
Tzah Mazuz <tzah.mazuz@testproject.io>
2021-03-29 12:51:39 +0300

    style: removing empty line
    Signed-off-by: Tzah Mazuz <tzah.mazuz@testproject.io>

--- src/testproject/enums/reportnamingelement.py
+++ src/testproject/enums/reportnamingelement.py
@@ -18,7 +18,6 @@ from enum import Enum, unique
 @unique
 class ReportNamingElement(Enum):
     """Enum containing types of report elements that can be inferred"""
-
     Project = 1
     Job = 2
     Test = 3
